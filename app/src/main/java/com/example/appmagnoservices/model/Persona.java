d746b71f5371968f7b8c1bece0a5b75c8fc3176c
Janssen Espinoza <jj147.e70@gmail.com>
2022-06-22 15:37:38 -0500

    modelo para la persona

---
+++ app/src/main/java/com/example/appmagnoservices/model/Persona.java
@@ -0,0 +1,61 @@
+package com.example.appmagnoservices.model;
+
+public class Persona {
+
+    public String nombre;
+    public String numeroDocumento;
+    public String nombres;
+    public String apellidoPaterno;
+    public String apellidoMaterno;
+
+    public Persona(String nombre, String numeroDocumento, String nombres, String apellidoPaterno, String apellidoMaterno) {
+        this.nombre = nombre;
+        this.numeroDocumento = numeroDocumento;
+        this.nombres = nombres;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public Persona() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNumeroDocumento() {
+        return numeroDocumento;
+    }
+
+    public void setNumeroDocumento(String numeroDocumento) {
+        this.numeroDocumento = numeroDocumento;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+}
