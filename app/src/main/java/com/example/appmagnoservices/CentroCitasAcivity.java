925b63ffd227f8ef9382ca8dda29ecdfbb599a2d
roque.mayo.reynaldo <roque.mayo.reynaldo@gmail.com>
2022-06-21 19:05:37 -0500

    terminado

--- app/src/main/java/com/example/appmagnoservices/CentroCitasAcivity.java
+++ app/src/main/java/com/example/appmagnoservices/CentroCitasAcivity.java
@@ -355,10 +355,126 @@ public class CentroCitasAcivity extends AppCompatActivity {
         });
 
 
+        actualizarFallidos();
+
+        //timeador
+        TimerTask task= new TimerTask() {
+            @Override
+            public void run() {
+
+
+                Log.i("SE EJECUTO","MINUTO");
+                String hora  = new SimpleDateFormat("HH:mm").format(calendar.getTime()).toString();
+
+                if(hora.equals("8:00")){
+
+                    actualizarFallidos();
+
+                }
+
+                if(hora.equals("8:30")){
+                    actualizarFallidos();
+                }
+                if(hora.equals("9:30")){
+                    actualizarFallidos();
+                }
+                if(hora.equals("10:00")){
+                    actualizarFallidos();
+                }
+                if(hora.equals("10:30")){
+                    actualizarFallidos();
+                }
+                if(hora.equals("11:00")){
+                    actualizarFallidos();
+                }
+                if(hora.equals("11:30")){
+                    actualizarFallidos();
+                }
+                if(hora.equals("12:00")){
+                    actualizarFallidos();
+                }
+                if(hora.equals("12:30")){
+                    actualizarFallidos();
+                }
+                if(hora.equals("01:00")){
+                    actualizarFallidos();
+                }
+                if(hora.equals("01:30")){
+                    actualizarFallidos();
+                }
+                if(hora.equals("02:00")){
+                    actualizarFallidos();
+                }
+                if(hora.equals("02:30")){
+                    actualizarFallidos();
+                }
+            }
+        };
+
+
+        Timer timer = new Timer("Timer");
+        long delay = 60000;
+
+        timer.schedule(task,0,delay);
+
 
     }
 
+ public void actualizarFallidos() {
 
+
+        frStore.collection("Cita/"+firebaseUser.getUid()+"/MisCitas").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+
+                if (error!=null){
+
+                    return;
+
+                }
+
+                for (QueryDocumentSnapshot doc : value) {
+
+
+                    DateFormat formatoDate = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+
+                    String FechaHoraActual = formatoDate.format(calendar.getTime()).toString();
+
+                    String FechaHoraCita = doc.getString("FechaCita") +" "+doc.getString("Hora");
+
+
+
+                    Date date1 = null;
+                    Date date2 = null;
+                    try {
+                        date1 = formatoDate.parse(FechaHoraActual);
+                        date2 = formatoDate.parse(FechaHoraCita);
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+
+                    Log.i("FECHA GUARDADA",date1.toString());
+                    Log.i("FECHA GUARDADA",date2.toString());
+                    if (date1.compareTo(date2)>0 && doc.getString("Estado").equals("ACTIVO") ){
+
+                        doc.getReference().update("Estado","FALLIDO");
+
+                    }
+
+
+
+
+
+//                    Cita cta= new Cita(doc.getString("Estado"),doc.getString("FechaCita"),
+//                            doc.getString("FechaCreacion"), doc.getString("Hora"),
+//                            doc.getString("Servicio"),doc.getString("Sucursal"),doc.getString("Descripcion") );
+//
+//                    frCitas.add(cta);
+                }
+
+            }
+        });
+    }
 
 
 }
